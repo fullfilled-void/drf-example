4e0a339603a84e4c663c61954c0b33cfe2b20b6b
fullfilled-void <fullfilled.void@gmail.com>
2021-08-19 01:11:53 +0900

    Add user view permission.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from accounts.serializers import UserSerializer
 
@@ -7,3 +8,4 @@ from accounts.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
